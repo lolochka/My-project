b43c69f2aeb3347682cfd9b35bbc68a3ad44e0a1
lolochka <ooshkoda@gmail.com>
2015-06-19 22:35:52 +0300

    1

--- myscript.js
+++ myscript.js
@@ -1,57 +1,30 @@
-//clean button
-clean = function () {
-    var ul = document.getElementById('list');//get ul in DOM
-    while (ul.hasChildNodes()) {//while ul has childs remove first
-        ul.removeChild(ul.firstChild);
-    }
+var sendComment = document.getElementById('send-comment');
+
+function Comment(text, author, date) {
+  this.text = text.value;
+  this.date = date.toUTCString();
+  this.author = author;
+}
+
+sendComment.onclick = function (e) {
+  var text = document.querySelector('textarea[name="comment"]');
+  var date = new Date();
+  var comment = new Comment(text, "Albus Dambledore", date);
+  storeComment(comment);
+  var commentBlock = document.getElementById('comments-block');
+  commentBlock.innerHTML += '<div class="block-emploee-description_block-comments_comment"><p class="block-emploee-description_block-comments_author">' + comment.author + '</p><p class="block-emploee-description_block-comments_comment-time" ><time datetime="2015-04-05">' + comment.date + '</time></p><p class="block-emploee-description_block-comments_comment-text">' + comment.text + '</p></div>';
+  return false;
 };
 
-//append button
-append = function () {
-    var i = 0; 
-    while (i < 100) {
-        i++;
-        var ul = document.getElementById('list'); 
-        var li = document.createElement('li');//create li
-        li.appendChild(document.createTextNode('Element '+i));//create li text
-        li.setAttribute('class', 'append');//create li class="append"
-        ul.appendChild(li);//place li in ul
-    }
-};
+function isOnline() {
+	return navigator.onLine; // перевіляє чи є доступ до інтернету
+}
 
-//inner button
-inner = function () {    
-    var ul = document.getElementById('list');
-    for (i = 1; i <= 100; i++) {
-        ul.innerHTML += '<li class="inner">Element ' + i + '</li>';//create html in ul with li and class
-    }
-};
+function storeComment(comment) {
+	if (isOnline()) {
+		storeCommentRemote(comment);
+	} else {
+		storeCommentLocal(comment);
+	}
+}
 
-//fragment button
-fragment = function () {
-    var i = 0;
-    while (i < 100) {
-        i++;
-        var ul = document.getElementById('list');
-        var d = document.createDocumentFragment();//create doc fragment
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode('Element '+i));//add text to li
-        li.setAttribute('class', 'fragment');//set class of li
-        d.appendChild(li);//add li as a child of doc fragment
-        ul.appendChild(d);//add doc fragment to ul
-    }
-};
-
-//join button
-join = function() {
-    var ul = document.getElementById('list');
-    var i = 0;
-    var list = [];//create array
-    do {
-        i++;
-        list.push('<li class="join">Element ');
-        list.push(i);    
-        list.push('</li>');
-    } while (i < 100);
-    ul.innerHTML += list.join('');
-};
