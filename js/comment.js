4874d5572ae8a21cab1141867040812dc80b7602
lolochka <ooshkoda@gmail.com>
2015-07-02 00:28:16 +0300

    1

--- js/comment.js
+++ js/comment.js
@@ -2,15 +2,15 @@ var commentButton = document.getElementById('send-comment');
 
 commentButton.onclick = function(e) {
 //  var emplAbout = document.querySelector('.active');
-  storeComment();
+  var id = document.querySelector('.active').id;
+  var currentEmpl = JSON.parse(localStorage.getItem(id));
+  storeComment(id, currentEmpl);
   document.getElementById('comment-form').reset();
   return false;
 }
 
-function storeComment() {
-  var id = document.querySelector('.active').id;
-  var currentEmpl = JSON.parse(localStorage.getItem(id));
-  console.log(currentEmpl);
+function storeComment(emplId, empl) {
+  console.log(empl);
   var comment = {};
   var text = document.querySelector('textarea[name="comment"]');
   var username = 'Myname';
@@ -18,7 +18,7 @@ function storeComment() {
   comment.text = text.value;
   comment.username = username;
   comment.date = date;
-  currentEmpl.comments.push(comment);
-  console.log(currentEmpl.comments);
-  addItem(id, currentEmpl);
+  empl.comments.push(comment);
+  console.log(empl.comments);
+  addItem(emplId, empl);
 }
